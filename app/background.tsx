7074c2a98c087d02d89e9261903be14edcb348fc
Sanjay <sanjay.india96@gmail.com>
2024-05-29 09:19:56 -0500

    Fixing Experience tab

--- app/background.tsx
+++ app/background.tsx
@@ -44,6 +44,7 @@ export default function background() {
           bucket list.
         </p>
         {/* <Link className="status" href="/resume.pdf" target="_blank"> */}
+        <Link className="status" href="" target="">
           <div className="status__light">
             <div className="status__light__ring"></div>
             <div className="status__light__led"></div>
@@ -51,7 +52,7 @@ export default function background() {
           <div className="status__message">
             Currently seeking full-time opportunities!
           </div>
-        {/* </Link> */}
+        </Link>
       </div>
     </section>
   );
