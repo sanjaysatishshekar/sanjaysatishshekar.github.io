2bf0e3f2648f8fe4764d6bd4afb51eb14cded6d4
Sanjay <sanjay.india96@gmail.com>
2024-05-29 11:52:47 -0500

    Job status update

--- app/experience_data.ts
+++ app/experience_data.ts
@@ -2,13 +2,19 @@ export default [
   {
     company: 'University of Texas at Arlington',
     url: 'https://www.uta.edu/',
-    time: 'August 2023 - Present',
+    time: 'August 2023 - May 2024',
     position: 'Graduate Teaching Assistant',
   },
   {
     company: 'Infosys Limited',
     url: 'https://www.infosys.com/',
-    time: 'July 2018 - July 2022',
-    position: 'Specialist Programmer',
+    time: 'June 2020 - July 2022',
+    position: 'Software Engineer II',
+  },
+  {
+    company: 'Infosys Limited',
+    url: 'https://www.infosys.com/',
+    time: 'July 2018 - June 2020',
+    position: 'Software Engineer I',
   },
 ]
