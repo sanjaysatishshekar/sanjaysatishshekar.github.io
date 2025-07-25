3707668360356c18f3f0192169794334df549274
Sanjay S S <sanjay.india96@gmail.com>
2025-07-25 10:01:57 -0700

    Update job status

--- app/experience_data.ts
+++ app/experience_data.ts
@@ -1,20 +1,26 @@
 export default [
   {
+    company: 'TechClub Inc',
+    url: '',
+    time: 'May 2025 - Present',
+    position: 'Software Engineer',
+  },
+  {
+    company: 'Sear Labs',
+    url: 'https://www.uta.edu/',
+    time: 'July 2024 - May 2025',
+    position: 'Research Volunteer',
+  },
+  {
     company: 'University of Texas at Arlington',
     url: 'https://www.uta.edu/',
     time: 'August 2023 - May 2024',
     position: 'Graduate Teaching Assistant',
   },
   {
-    company: 'Infosys Limited',
-    url: 'https://www.infosys.com/',
-    time: 'June 2020 - July 2022',
-    position: 'Software Engineer II',
-  },
-  {
-    company: 'Infosys Limited',
-    url: 'https://www.infosys.com/',
-    time: 'July 2018 - June 2020',
-    position: 'Software Engineer I',
-  },
+    company: "Apple Inc",
+    url: 'https://apple.com/',
+    time: 'July 2018 - July 2024',
+    position: 'Software Engineer (Consultant)',
+  }
 ]
